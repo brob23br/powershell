f9280ee46cc859ec09922844c8f0c907f3202369
brob23br <152234119+brob23br@users.noreply.github.com>
2025-06-07 21:01:38 -0400

    Update prompts.ps1

--- prompts.ps1
+++ prompts.ps1
@@ -1,4 +1,19 @@
-# Name: Brandon Robinson | Student ID: 001260410 | Class: D411 Scripting and Automation
+    <# 
+    .NOTES
+    Name: Brandon Robinson | Student ID: 001260410 | Class: D411 Scripting and Automation
+    
+    .SYNOPSIS
+    Create a “switch” statement that continues to prompt a user by doing each of the following activities, until a user presses key 5:
+
+    1.  Using a regular expression, list files within the Requirements1 folder, with the .log file extension and redirect the results to a new file called “DailyLog.txt” within the same directory without overwriting existing data. 
+            Each time the user selects this prompt, the current date should precede the listing. (User presses key 1.)
+    2.  List the files inside the “Requirements1” folder in tabular format, sorted in ascending alphabetical order. 
+            Direct the output into a new file called “C916contents.txt” found in your “Requirements1” folder. (User presses key 2.)
+    3.  List the current CPU and memory usage. (User presses key 3.)
+    4.  List all the different running processes inside your system. Sort the output by virtual size used least to greatest, and display it in grid format. (User presses key 4.)
+    5.  Exit the script execution. (User presses key 5.)
+
+    #>
 
 # Define menuDisplay function to display menu 
 function menuDisplay {
